deb1e4415c684f7f1e680479658f5962d2902a89
Nishanth Anand <nishanthanand817@gmail.com>
2024-02-01 12:14:43 +0530

    ah javascript

---
+++ terminal-portfolio/src/components/Projects.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Projects = () => {
+    return (
+        <div className="terminal">
+            <h1>Projects</h1>
+            <ul>
+                <li>Project 1</li>
+                <li>Project 2</li>
+                <li>Project 3</li>
+            </ul>
+        </div>
+    );
+};
+
+export default Projects;
